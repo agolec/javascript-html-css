8e2f7202c209f40815f192084de6f4485c872c36
Adam Golec <agolec@nexient.com>
2023-08-10 17:13:21 -0400

    add guess my number directory to project

--- guess-my-number/script.js
+++ guess-my-number/script.js
@@ -3,4 +3,11 @@
 console.log(document.querySelector('.message').
     textContent);
 
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+console.log(document.querySelector('.message').
+    textContent);
     
+    document.querySelector('.number').textContent = '13';
+    document.querySelector('.score').textContent = '11';
+    console.log(document.querySelector('.guess').value = '12')
