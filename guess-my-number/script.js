4c7619de15d300a309589344ea50735e5903afb8
Adam Golec <agolec@nexient.com>
2023-08-11 12:23:33 -0400

    add event listener for click action on .check class element and code to change the .message class element to display text for the user when their guess is empty text/nothing.

--- guess-my-number/script.js
+++ guess-my-number/script.js
@@ -1,13 +1,11 @@
 'use strict';
 
-console.log(document.querySelector('.message').
-    textContent);
+//Use the querySelector method to specify
+document.querySelector('.check').addEventListener('click', function(){
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(typeof guess);
 
-document.querySelector('.message').textContent = '🎉 Correct Number!';
-
-console.log(document.querySelector('.message').
-    textContent);
-    
-    document.querySelector('.number').textContent = '13';
-    document.querySelector('.score').textContent = '11';
-    console.log(document.querySelector('.guess').value = '12')
+    if(!guess){
+        document.querySelector('.message').textContent = '⛔No Number entered';
+    }
+})
