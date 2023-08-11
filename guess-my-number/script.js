6ab4a161486f0ab7ec39c1835c7d4d72b261581c
Adam Golec <agolec@nexient.com>
2023-08-11 19:23:33 -0400

    set up base code for game. Left off on lecture 75 on Udemy.

--- guess-my-number/script.js
+++ guess-my-number/script.js
@@ -1,5 +1,9 @@
 'use strict';
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1
+let score = 20
+//document.querySelector('.number').textContent = secretNumber;
+console.log(secretNumber);
 //Use the querySelector method to specify
 document.querySelector('.check').addEventListener('click', function(){
     const guess = Number(document.querySelector('.guess').value);
@@ -8,4 +12,31 @@ document.querySelector('.check').addEventListener('click', function(){
     if(!guess){
         document.querySelector('.message').textContent = '⛔No Number entered';
     }
+    else if(guess === secretNumber){
+        score--
+        document.querySelector('.message').textContent = '⚖Correct number!';
+        document.querySelector('.score').textContent = score;
+    } 
+    else if (guess > secretNumber) {
+        if(score > 1){
+            document.querySelector('.message').textContent = '📈Guess too high.';
+            score--;
+            document.querySelector('.score').textContent = score;
+        }
+        else{
+            document.querySelector('.score').textContent = 0;
+            document.querySelector('.message').textContent = '💥You lost the game';
+        }
+        
+    } else if (guess < secretNumber){
+        if(score > 1){
+            document.querySelector('.message').textContent = '📉Guess too low.'
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.score').textContent = 0;
+            document.querySelector('.message').textContent = '💥You lost the game';
+        }
+        
+    } 
 })
