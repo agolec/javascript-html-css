c98ca14cbb8fc97c5f91f4ab93306e4d02531a65
Adam Golec <agolec@nexient.com>
2023-08-16 18:41:43 -0400

    clean up code so that score does not decrement if user guesses correct number. Score is only decremented upon an incorrect guess submitted by the user. Therefore max score possible is 20.

--- guess-my-number/script.js
+++ guess-my-number/script.js
@@ -1,9 +1,12 @@
 'use strict';
 
-let secretNumber = Math.trunc(Math.random() * 20) + 1
-let score = 20
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0; 
 
 console.log(secretNumber);
+console.log('high score set at start: ' + highScore);
+console.log('type of: ' + typeof highScore);
 //Use the querySelector method to make an event listener on the
 //check guess button.
 document.querySelector('.check').addEventListener('click', function(){
@@ -14,13 +17,20 @@ document.querySelector('.check').addEventListener('click', function(){
         document.querySelector('.message').textContent = '⛔No Number entered';
     }
     //Player wins
+
+    //set high score if score > the current high score.
     else if(guess === secretNumber){
-        score--
         document.querySelector('.message').textContent = '⚖Correct number!';
         document.querySelector('body').style.backgroundColor = '#60b347';
         document.querySelector('.number').style.width = '30rem';
 
         document.querySelector('.number').textContent = secretNumber;
+        document.querySelector('.score').textContent = score;
+       
+        if(score > highScore){
+            highScore = score;
+            document.querySelector('.highscore').textContent = highScore;
+        }
     } 
     //User guess is too high
     else if (guess > secretNumber) {
@@ -56,7 +66,7 @@ document.querySelector('.again').addEventListener('click',function(){
     //Guess too low/high text reset.
     //Score label reset.
     //High Schore label reset.
-    
+
     secretNumber = Math.trunc(Math.random() * 20) + 1;
 
     score = 20;
