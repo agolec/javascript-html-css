ad40fd4bbe6a0db6584f9c6a7c146e345c623307
Adam <algolec1990@yahoo.com>
2023-08-18 17:10:39 -0400

    turn openModal and closeModal actions in to named functions and call those in eventListeners.

--- modal-window-project/script.js
+++ modal-window-project/script.js
@@ -2,22 +2,22 @@
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.close-modal')
+const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-let count = 0;
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
 
-for(let i = 0; i < btnsOpenModal.length;i++){
-    btnsOpenModal[i].addEventListener('click',function(){
-        count++;
-        console.log('Button clicked ' + count + ' times.');
-        modal.classList.remove('hidden');
-        overlay.classList.remove('hidden');
-        
-    });
-    btnCloseModal.addEventListener('click',function(){
-        modal.classList.add('hidden');
-        overlay.classList.add('hidden');
-    })
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+  btnCloseModal.addEventListener('click', closeModal);
+  overlay.addEventListener('click', closeModal);
 }
-
