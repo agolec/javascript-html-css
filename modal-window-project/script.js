16b0ba3dd2e9bc7f0ec3ede46c3420318cb5da0e
Adam Golec <agolec@nexient.com>
2023-08-18 15:19:08 -0400

    Add code to hide and display modal window in demo program.

--- modal-window-project/script.js
+++ modal-window-project/script.js
@@ -5,7 +5,19 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal')
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
+let count = 0;
+
 for(let i = 0; i < btnsOpenModal.length;i++){
-    console.log(btnsOpenModal[i].textContent);
+    btnsOpenModal[i].addEventListener('click',function(){
+        count++;
+        console.log('Button clicked ' + count + ' times.');
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+        
+    });
+    btnCloseModal.addEventListener('click',function(){
+        modal.classList.add('hidden');
+        overlay.classList.add('hidden');
+    })
 }
 
